9ca9aef4f1ab6f43a81804947783f116f1b0aed6
aoulaa <olimbek.nizomov95@gmail.com>
2022-11-20 12:00:32 +0500

    tokens

--- tests/auto_enhance_methods.py
+++ tests/auto_enhance_methods.py
@@ -10,7 +10,7 @@ class AutoEnhanceTestCase(unittest.TestCase):
     def test_get_img_status(self):
 
         success_response = {
-            'image_id': '52ceddc6-1940-485a-98bf-2d122e71806c',
+            'image_id': '52ceddcds6-1940-sd485a-98bf-2dsd122e71806c',
             'image_name': 'image_homef.png',
             'image_type': 'jpeg',
             'enhance_type': 'property',
@@ -23,28 +23,28 @@ class AutoEnhanceTestCase(unittest.TestCase):
             'vibrant': False
         }
 
-        result = self.auto_enhance.get_img_status(image_id='52ceddc6-1940-485a-98bf-2d122e71806c')
+        result = self.auto_enhance.get_img_status(image_id='52ceddc6-194ds0-485a-ds-2d122e71806c')
         self.assertEqual(result, success_response)
 
     def test_preview_enhanced_img(self):
 
         success_response = 200
 
-        result = self.auto_enhance.preview_enhanced_img(image_id='52ceddc6-1940-485a-98bf-2d122e71806c')
+        result = self.auto_enhance.preview_enhanced_img(image_id='52ceddc6sd-1940-485a-98bf-ds')
         self.assertEqual(result.get('status'), success_response)
 
     def test_web_optimised_img(self):
 
         success_response = 200
 
-        result = self.auto_enhance.web_optimised_img(image_id='52ceddc6-1940-485a-98bf-2d122e71806c')
+        result = self.auto_enhance.web_optimised_img(image_id='sdds-1940-485a-ds-2d122e71806c')
         self.assertEqual(result.get('status'), success_response)
 
     def test_full_resol_enhanced_img(self):
 
         success_response = 200
 
-        result = self.auto_enhance.full_resol_enhanced_img(image_id='52ceddc6-1940-485a-98bf-2d122e71806c')
+        result = self.auto_enhance.full_resol_enhanced_img(image_id='sdsdsds-1940-48ds5a-98bf-sd')
         self.assertEqual(result.get('status'), success_response)
 
     if __name__ == '__main__':
